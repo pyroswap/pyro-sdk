0e964da2a2c7b0f8a2b9f67480e6d596bfdcc18d
pyroswap <pyroswap@protonmail.com>
2021-06-01 06:12:22 +0200

    Update for testnet

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -59,16 +59,16 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
 export const WETH = {
   [ChainId.MAINNET]: new Token(
     ChainId.MAINNET,
-    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
     18,
-    'WBNB',
-    'Wrapped BNB'
+    'WETH',
+    'Wrapped ETH'
   ),
   [ChainId.BSCTESTNET]: new Token(
     ChainId.BSCTESTNET,
-    '0xaE8E19eFB41e7b96815649A6a60785e1fbA84C1e',
+    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
     18,
-    'WBNB',
-    'Wrapped BNB'
+    'WETH',
+    'Wrapped ETH'
   )
 }
